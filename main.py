5c83dbe05c616b156fa2d56b4a04e3151d21aade
FeriPratama <117093475+CyberCarboon2@users.noreply.github.com>
2022-12-29 21:18:23 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# Mau Recode Yaaaa???
+# Silahkan Bree Izin Dulu Tapi Lah
+# Feri Pratama
+# CyberCarboon2
+#--------------------------[ MODULE ]---------------------
+import os,sys
+import Feri
+
+def create_dir():
+	try:
+		os.mkdir('OK')
+	except:
+		pass
+	try:
+		os.mkdir('CP')
+	except:pass
+
+def __feri__():
+	Feri.false()
+	
+if __name__=="__main__":
+	create_dir()
+	__feri__()
