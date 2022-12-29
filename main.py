ee776b6c0605e01931d1af2d6c8f627459779352
FeriPratama <117093475+CyberCarboon2@users.noreply.github.com>
2022-12-29 14:16:12 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Mau Recode Yaaaa???
+# Silahkan Bree Izin Dulu Tapi Lah
+# Feri Pratama
+# CyberCarboon2
+#--------------------------[ MODULE ]---------------------
+import os,sys
+import Feri
+
+def __feri__():
+	Feri.false()
+	
+if __name__=="__main__":
+	__feri__()
